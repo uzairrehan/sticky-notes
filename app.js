5c0ad5b1e45ae3f5eb9f5ee3366b9ee5d7cf6665
Syed Uzair Shah <iamsyeduzairshah@gmail.com>
2024-05-16 21:34:33 +0500

    created todo app

---
+++ app.js
@@ -0,0 +1,35 @@
+// CRUD // create read update delete
+
+let input = document.querySelectorAll("input");
+let todoListDiv = document.querySelectorAll(".output");
+let allTodos = [];
+
+function add() {
+  let todoVal = input[0].value;
+  allTodos.push(todoVal);
+  printing();
+  input[0].value = "";
+}
+
+function printing() {
+  todoListDiv[0].innerHTML = "";
+  for (let i = 0; i < allTodos.length; i++) {
+    todoListDiv[0].innerHTML += `
+            <p id="todo-${i}">
+                ${allTodos[i]}
+                <button onclick="editTodo(${i}) "class="material-symbols-outlined button ">Edit</button>
+                <button onclick="deleteTodo(${i})"class="material-symbols-outlined button ">Delete</button>
+            </p>
+            <br>
+        `;
+  }
+}
+
+function deleteTodo(i) {
+  allTodos.splice(i, 1);
+  printing();
+}
+
+function editTodo(i) {
+
+}
