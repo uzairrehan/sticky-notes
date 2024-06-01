faa7dd7f86b57f508b43e07ff5ee6e3deb12eecd
Syed Uzair Shah <iamsyeduzairshah@gmail.com>
2024-06-01 18:57:11 +0500

    edit function and all ok

--- app.js
+++ app.js
@@ -1,36 +1,92 @@
-let input = document.querySelectorAll("input");
+let todoInput = document.querySelectorAll(".new-todo")[0];
 let todoListDiv = document.querySelectorAll(".output");
+let errorPEl = document.querySelectorAll(".error")[0];
+let editTodoDiv = document.querySelectorAll("#edit-todo")[0];
+let addTodoDiv = document.querySelectorAll("#add-new-todo")[0];
+let editTodoInput = document.querySelectorAll(".edit-todo")[0];
+let editIndex;
 let allTodos = [];
+let isEditing = false;
+
+
+//  add function  
 function add() {
-  let todoVal = input[0].value;
-  if (todoVal === "") {
-    return;
-  }
+  let todoVal = todoInput.value;
   allTodos.push(todoVal);
-  printing();
-  input[0].value = "";
+  printAllTodos();
+  todoInput.value = "";
 }
-function printing() {
+
+
+// print on page
+function printAllTodos() {
   todoListDiv[0].innerHTML = "";
   for (let i = 0; i < allTodos.length; i++) {
     todoListDiv[0].innerHTML += `
-            <div id="todo-${i}">
-                <p>
-                ${allTodos[i]}
-                </p>
-                <span>(
-                ${i + 1} )
-                <button onclick="editTodo(${i}) "class="material-symbols-outlined button ">Edit</button>
-                <button onclick="deleteTodo(${i})"class="material-symbols-outlined button ">Delete</button>
-                </span>
-                </div>
-        `;
+              <div id="todo-${i}">
+                    <p>
+                      ${allTodos[i]}
+                    </p>
+                    <span>(${i + 1})
+                       <button onclick="editTodo(${i}) "class="material-symbols-outlined button ">Edit</button>
+                       <button onclick="deleteTodo(${i})"class="material-symbols-outlined button ">Delete</button>
+                    </span>
+              </div>
+                `;
   }
 }
-function deleteTodo(i) {
-  allTodos.splice(i, 1);
-  printing();
+
+
+// delete fun
+function deleteTodo(index) {
+  allTodos.splice(index, 1);
+  printAllTodos();
 }
-function editTodo(i) {
-  printing();
+
+
+
+// will edit
+function editTodo(index) {
+  if (isEditing) {
+    displayError();
+  } else {
+    editIndex = index;
+    toggleTodoForm();
+    editTodoInput.value = allTodos[index];
+  }
+}
+
+
+
+
+// update function on page
+function upadateTodo() {
+  toggleTodoForm();
+  allTodos.splice(editIndex, 1, editTodoInput.value);
+  printAllTodos();
+}
+
+
+
+// this funs is hiding save div 
+function toggleTodoForm() {
+  if (!isEditing) {
+    editTodoDiv.className = "";
+    addTodoDiv.className += " hide";
+  } else {
+    editTodoDiv.className += " hide";
+    addTodoDiv.className = "";
+  }
+  isEditing = !isEditing;
+}
+
+
+
+
+// error function will show if not saved
+function displayError() {
+  errorPEl.innerHTML = "First save";
+  setTimeout(function () {
+    errorPEl.innerHTML = "";
+  }, 4000);
 }
